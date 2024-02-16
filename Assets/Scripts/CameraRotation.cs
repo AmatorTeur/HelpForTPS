3841d39f1a102d4541d9197402081476960bc239
AmatorTeur <jjj.aaa2005@mail.ru>
2024-02-16 21:34:38 +0300

    Axis

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public Transform CameraAxisTransform;
+    public float minAxis = 1;
+    public float maxAxis = 10;
+    public float _rotationSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * _rotationSpeed * Input.GetAxis("Mouse X"), 0);
+        var n = CameraAxisTransform.localEulerAngles.x + Time.deltaTime * _rotationSpeed * -Input.GetAxis("Mouse Y");
+        var newAxis = Mathf.Clamp(n, minAxis, maxAxis);
+        CameraAxisTransform.localEulerAngles = new Vector3(newAxis, 0, 0);
+    }
+}
