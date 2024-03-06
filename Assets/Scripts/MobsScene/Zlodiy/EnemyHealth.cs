2a4960ddfb8ba05371df6f6ea08575bd7627e878
AmatorTeur <jjj.aaa2005@mail.ru>
2024-03-06 23:01:04 +0300

    3Mod_3Ned
    i'm alive

---
+++ Assets/Scripts/MobsScene/Zlodiy/EnemyHealth.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float value;
+    public Fireball _fireballDamage;
+
+    void Start()
+    {
+        _fireballDamage = GetComponent<Fireball>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void DealDamage(float damage)
+    {
+        value -= damage;
+        if (value <= 0) Destroy(gameObject);
+    }
+
+/*    private void OnCollisionEnter(Collision collision)
+    {
+        DamageEnemy(collision);
+    }
+    private void DamageEnemy(Collision collision)
+    {
+        if (collision.gameObject.tag == "FireBall")
+            value -= 20;
+        //value -= _fireballDamage.damage;
+    }*/
+
+    /*    private void Death()
+        {
+            if (value <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }*/
+}
